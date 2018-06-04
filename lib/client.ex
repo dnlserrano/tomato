4d701e4281eaa760cd49960ae24e88d002d85299
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2018-06-05 00:27:38 +0100

    Now with cities and much better error handling

--- lib/client.ex
+++ lib/client.ex
@@ -1,22 +1,40 @@
 defmodule Tomato.Client do
-  @user_key System.get_env("ZOMATO_API_KEY")
-  @base_uri System.get_env("ZOMATO_API_URI")
+  @api_key System.get_env("ZOMATO_API_KEY")
+  @api_uri System.get_env("ZOMATO_API_URI")
 
   def get(path, parameters \\ %{}) do
-    %HTTPoison.Response{body: body} =
-      HTTPoison.get!(uri(path), add_user_key(parameters))
+    {:ok, %HTTPoison.Response{status_code: status_code, body: body}} =
+      HTTPoison.get(uri(path), headers(), options(parameters))
 
-    Poison.decode!(body, keys: :atoms)
+    case status_code < 300 do
+      true -> {:ok, Poison.decode!(body, keys: :atoms)}
+      false -> {:error, Poison.decode!(body, keys: :atoms)}
+    end
   end
 
   defp uri(path) do
-    @base_uri
+    @api_uri
     |> URI.merge(path)
     |> URI.to_string()
   end
 
-  defp add_user_key(parameters) do
-    parameters
-    |> Map.put(:user_key, @user_key)
+  defp headers() do
+    [
+      {"Accept", "application/json"},
+      {"X-Zomato-API-Key", @api_key}
+    ]
+  end
+
+  defp options(parameters) do
+    query_params = build_query_params(parameters)
+
+    [
+      params: query_params
+    ]
+  end
+
+  defp build_query_params(parameters) do
+    :maps.filter(fn _, v -> v != nil && v != "" end, parameters)
+    |> Map.to_list
   end
 end
--- lib/tomato.ex
+++ lib/tomato.ex
@@ -1,24 +1,50 @@
 defmodule Tomato do
-  alias Tomato.Client
   @moduledoc """
   Documentation for Tomato.
   """
+
+  alias Tomato.Client
 
   @doc """
   Get categories
   """
   def categories do
-    Client.get("categories")[:categories]
-    |> Enum.map(fn(category) ->
-      struct(Tomato.Category, category[:categories])
-    end)
+    with {:ok, response} <- Client.get("categories") do
+      categories =
+        response
+        |> Map.get(:categories)
+        |> Enum.map(fn(category) ->
+          struct(Tomato.Category, category[:categories])
+        end)
+
+      {:ok, categories}
+    else
+      error -> error
+    end
   end
 
-  def cities(city_query) do
-    city_params = city_query |> Map.delete(:__struct__)
-    Client.get("cities", city_params)[:location_suggestions]
-    |> Enum.map(fn(city) ->
-      struct(Tomato.City, city[:cities])
-    end)
+  @doc """
+  Get cities
+  """
+  def cities(%Tomato.Queries.City{} = city_query) do
+    city_params = query_to_params(city_query)
+
+    with {:ok, response} <- Client.get("cities", city_params) do
+      cities =
+        response
+        |> Map.get(:location_suggestions)
+        |> Enum.map(fn(city) ->
+          struct(Tomato.City, city)
+        end)
+
+      {:ok, cities}
+    else
+      error -> error
+    end
+  end
+
+  defp query_to_params(query) do
+    query
+    |> Map.from_struct
   end
 end
